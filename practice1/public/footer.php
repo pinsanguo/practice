a4a6e0f848be2df5114e5a88b39ce0e5f2b3f949
wangsen <1356053990@qq.com>
2019-06-22 18:52:57 +0800

    666

---
+++ practice1/public/footer.php
@@ -0,0 +1,10 @@
+<script src="/public/layuiadmin/layui/layui.js"></script>
+<script>
+    layui.config({
+        base: '/layuiadmin/' //静态资源所在路径
+    }).extend({
+        index: 'lib/index' //主入口模块
+    }).use('index');
+</script>
+</body>
+</html>
