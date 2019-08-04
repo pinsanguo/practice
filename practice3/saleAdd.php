34f0e3827bfb2c784e96ded10b297b79c5b0d86e
pinsanguo <1356053990@qq.com>
2019-08-04 12:36:09 +0800

    666

--- practice3/saleAdd.php
+++ practice3/saleAdd.php
@@ -30,7 +30,6 @@ if(!empty($_POST['sale_name'])){
         ]);
     //拿货200支，自动成为总裁。拿货30支，自动成为联合创始人。拿货5支自动成为合伙人。
     if($post['number'] >= 200){
-        echo $userId;die();
         $mysql->where(array('id'=>$userId))->update('user',
             [
                 'title'=>'总裁',
